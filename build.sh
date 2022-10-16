035e8d2b10fbfb43348a422bd4f6fab58af9da90
zacky <103025109+zacky46@users.noreply.github.com>
2022-10-16 13:40:42 +0700

    Build: NusantaraProject A10

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-project/local_manifest --depth 1 -b banana-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export NFS_TARGET_BUILD_ROM=bananadroid
+export NFS_TARGET_BUILD_ROM=NusantaraProject
 source build/envsetup.sh
-lunch banana_ginkgo-user
+lunch nad_ginkgo-userdebug
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=zacky
+export KBUILD_BUILD_USER=yanzszs
 export KBUILD_BUILD_HOST=nfsproject
-export BUILD_USERNAME=zacky
+export BUILD_USERNAME=yanzszs
 export BUILD_HOSTNAME=nfsproject
-mka banana -j8
+mka nad -j8
 # Switch to vanilla build
