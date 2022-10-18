dbf286b1390a6743a1b401752750e077f6ad5677
zacky <103025109+zacky46@users.noreply.github.com>
2022-10-18 16:23:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yanzszs/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b whyred-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export NFS_TARGET_BUILD_ROM=NusantaraProject
+export NFS_TARGET_BUILD_ROM=RiceDroid
 source build/envsetup.sh
-lunch nad_ginkgo-userdebug
+lunch lineage_whyred-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=nfsproject
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=nfsproject
-mka nad -j8
-# Build NusantaraProject With GApps
+mka bacon -j8
+# Build RiceDroid Thirteen
