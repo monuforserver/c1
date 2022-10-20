5e5ee8653b305c08ccb8ab6f5f8f0ff45926fa0d
T.B.S <ewprjkt@proton.me>
2022-10-20 22:00:35 +0700

    Turu

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ export NFS_TARGET_BUILD_ROM=RiceDroid
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export TZ=Asia/Shanghai
-export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
+#export ALLOW_MISSING_DEPENDENCIES=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
 export KBUILD_BUILD_USER=dotkit
-export KBUILD_BUILD_HOST=NFS-Project
+#export KBUILD_BUILD_HOST=NFS-Project
 export BUILD_USERNAME=dotkit
-export BUILD_HOSTNAME=NFS-Project
+#export BUILD_HOSTNAME=NFS-Project
 mka bacon -j8
 # Build RiceDroid Thirteen Vanilla
