82bde2852923d8d68364ed0b6d0da275527818b9
T.B.S <ewprjkt@proton.me>
2022-10-20 17:32:12 +0700

    test om

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yanzszs/local_manifest --depth 1 -b whyred-13 .repo/local_manifests
+git clone https://github.com/Tiktodz/local_manifest --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 export NFS_TARGET_BUILD_ROM=RiceDroid
 source build/envsetup.sh
-lunch lineage_whyred-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
