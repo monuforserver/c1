61891dcb0c2a633f80898f20037ae605979d71dd
T.B.S <ewprjkt@proton.me>
2022-10-22 00:59:54 +0700

    regen [skip ci]

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b rice .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifest --depth 1 -b cipher .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export NFS_TARGET_BUILD_ROM=RiceDroid
+#export NFS_TARGET_BUILD_ROM=CipherOS
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export TZ=Asia/Shanghai
 #export ALLOW_MISSING_DEPENDENCIES=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
-export KBUILD_BUILD_USER=dotkit
 #export KBUILD_BUILD_HOST=NFS-Project
 export BUILD_USERNAME=dotkit
 #export BUILD_HOSTNAME=NFS-Project
