8831563ea691837a05f5bdb9d8c493dd1035352b
T.B.S <ewprjkt@proton.me>
2022-10-25 21:09:54 +0700

    reload

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b sup .repo/local_manifests
+git clone https://github.com/Tiktodz/local_manifest --depth 1 -b crd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
