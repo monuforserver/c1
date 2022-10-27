8f6ad343c627395f3fcba72dba43ed1adf0420c5
T.B.S <ewprjkt@proton.me>
2022-10-28 00:00:56 +0700

    make xd

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b crd .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifest --depth 1 -b xd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch superior_X00TD-userdebug
+lunch xdroid_X00TD-userdebug
 export TZ=Asia/Shanghai
 #export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-m bacon -j8
+#export SELINUX_IGNORE_NEVERALLOWS=true
+make xd -j8
 # end
