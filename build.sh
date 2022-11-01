e7449afd02f5fa53573bc6c90fc48e7eb7821b94
T.B.S <ewprjkt@proton.me>
2022-11-01 19:57:50 +0700

    colt

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b crd .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Colt-Enigma/platform_manifest -b c13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifest --depth 1 -b radiant .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00TD-userdebug
-export TZ=Asia/Makassar
+lunch colt_X00TD-userdebug
+export TZ=Asia/Jakarta
 #export ALLOW_MISSING_DEPENDENCIES=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
-m bacon -j8
+make bacon -j8
 # end
