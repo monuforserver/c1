66f015de88722b23e13dfc6aa5e8f2029cd8da03
T.B.S <ewprjkt@proton.me>
2022-11-03 03:53:10 +0700

    initial blaze

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b xd .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifest --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xdroid_X00TD-userdebug
+lunch blaze_X00TD-userdebug
 export TZ=Asia/Jakarta
 #export ALLOW_MISSING_DEPENDENCIES=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
-make xd -j8
+mka bacon -j8
 # end
