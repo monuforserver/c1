291a9bfcf17b4588a729c4dbfb0ca1640a23b71a
T.B.S <ewprjkt@proton.me>
2022-11-06 00:32:13 +0700

    second blaze [skip ci]

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b rice .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifest --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_X00TD-userdebug
+lunch blaze_X00TD-userdebug
 export TZ=Asia/Jakarta
 #export ALLOW_MISSING_DEPENDENCIES=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
-mka bacon
+mka bacon -j8
 # end
