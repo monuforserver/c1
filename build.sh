76029f5763567dfba56285550d5a2c82f07d93a7
monuforserver <111532413+monuforserver@users.noreply.github.com>
2022-11-18 06:33:18 +0530

    Try

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b xr .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/god-goku/local-manifest.git --depth 1 -b cherry .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch xtended_X00TD-userdebug
-export TZ=Asia/Jakarta
-export SKIP_API_CHECKS=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-make bacon -j8
-# end
+lunch cherish_veux-userdebug
+export TZ=Asia/Kolkata
+#export CHERISH_VANILLA := true
+brunch veux 
+#end1
